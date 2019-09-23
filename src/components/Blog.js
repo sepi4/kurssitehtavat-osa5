a4b10fe62e063102444d3f9241ee369ef381b8e8
Sergei <sepolish@student.jyu.fi>
2019-09-23 19:37:13 +0300

    5.6* blogilistan frontend, step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,42 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const divStyle = {
+    backgroundColor: "#E0E0E0",
+    padding: "1px 15px",
+    margin: "10px 0",
+  }
+  return (
+    <div>
+
+      {showAll 
+          ? 
+            <div style={divStyle}>
+              <h2 style={{cursor: "pointer"}} onClick={() => {
+                setShowAll(!showAll)
+              }}>
+                {blog.title}
+              </h2>
+              <p>author: {blog.author}</p>
+              <p>added by: {blog.user.name ? blog.user.name : blog.user.username}</p>
+              <p>url: <a href={blog.url}>{blog.url}</a></p>
+              <p>likes: {blog.likes} <button>like</button></p>
+            </div>
+          :
+            <div>
+              <p 
+                style={{cursor: "pointer"}} 
+                onClick={() => {
+                  setShowAll(!showAll)
+                }}
+              >
+                <strong>{blog.title}</strong>, by {blog.author}
+              </p>
+            </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
