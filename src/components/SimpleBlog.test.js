99e597d34b3b34795217536db075abf29193a306
Sergei <sepolish@student.jyu.fi>
2019-09-24 18:35:19 +0300

    5.14: blogilistan testit, step2

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,33 +1,43 @@
-
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
 
-test('renders content', () => {
+describe('testit', () => {
+  let component
   const blog={
     title: 'Simple blog title',
     author: 'Sergei',
     likes: 0,
   }
-  const onClick = () => {
-    console.log('click')
-  }
+  const mockHandler = jest.fn()
 
-  const component = render(
-    <SimpleBlog
-      blog={blog}
-      onClick={onClick}
-    />
-  )
+  beforeEach(() => {
+    component = render(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+  })
 
+  test('component render: title, author, likes', () => {
+    expect(component.container).toHaveTextContent(
+      `${blog.title} ${blog.author}`
+    )
+    expect(component.container).toHaveTextContent(
+      `blog has ${blog.likes} likes`
+    )
+  })
 
-  expect(component.container).toHaveTextContent(
-    `${blog.title} ${blog.author}`
-  )
-  expect(component.container).toHaveTextContent(
-    `blog has ${blog.likes} likes`
-  )
+  test('if like button press 2 times, callback is called 2 times', () => {
+    const button = component.getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
+
 })
+
